441b791ca86acba4ac4a7609df21e6ddc58ff97e
Abbas Khan Mohammad <abbas.khan.mohammadd@gmail.com>
2023-10-13 12:24:32 +0530

    4

--- LECTURES/Arrays/PairsInArray.java
+++ LECTURES/Arrays/PairsInArray.java
@@ -2,12 +2,14 @@ package LECTURES.Arrays;
 
 public class PairsInArray {
     public static void pairsInArray(int arr[]){
+        int totalPairs = 0;
         for(int i=0; i<arr.length; i++){
             int curr = arr[i];
             for(int j=i+1; j<arr.length; j++){
                 System.out.print("("+curr+","+arr[j]+")");
+                totalPairs++;
             }System.out.println();
-        }
+        }System.out.println(totalPairs);
     }
     public static void main(String[] args) {
         int arr[] = {2,4,6,8,10};
