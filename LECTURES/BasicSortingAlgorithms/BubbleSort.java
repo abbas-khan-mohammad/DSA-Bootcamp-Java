7e342f9bdaf4e030656deac08f78c0a757418c80
Abbas Khan Mohammad <abbas.khan.mohammadd@gmail.com>
2023-10-17 10:55:17 +0530

    0

--- LECTURES/BasicSortingAlgorithms/BubbleSort.java
+++ LECTURES/BasicSortingAlgorithms/BubbleSort.java
@@ -19,7 +19,7 @@ public class BubbleSort {
         }System.out.println();
     }
     public static void main(String[] args) {
-        int arr[] = {1000,4,1,3,2};
+        int arr[] = {1000,99,1,3,2};
         bubbleSort(arr);
         printArr(arr);
     }
