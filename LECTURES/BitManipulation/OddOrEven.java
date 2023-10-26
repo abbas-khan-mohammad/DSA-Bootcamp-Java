442ac616d1e2a4ce6e3d6c0be32b7b00a65d35fd
Abbas Khan Mohammad <abbas.khan.mohammadd@gmail.com>
2023-10-26 15:42:17 +0530

    0

---
+++ LECTURES/BitManipulation/OddOrEven.java
@@ -0,0 +1,15 @@
+package LECTURES.BitManipulation;
+
+public class OddOrEven {
+    public static void oddOrEven(int n){
+        int bitMask = 1;
+        if((n & bitMask)==0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+    public static void main(String[] args) {
+        oddOrEven(9);
+    }
+}
