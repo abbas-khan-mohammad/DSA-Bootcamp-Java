5a3f2d17713253dfcff0c95c7340c6c3dfc5dda1
Abbas Khan Mohammad <abbas.khan.mohammadd@gmail.com>
2023-11-07 12:16:16 +0530

    1

--- LECTURES/Recursion/PrintNumbersInIncreasingOrder.java
+++ LECTURES/Recursion/PrintNumbersInIncreasingOrder.java
@@ -3,7 +3,7 @@ package LECTURES.Recursion;
 public class PrintNumbersInIncreasingOrder {
     public static void printNumbersInIncreasingOrder(int n){
         if(n==1){
-            System.out.print(n+" ");
+            System.out.print(n+" ");//increase
             return;
         }
         printNumbersInIncreasingOrder(n-1);
